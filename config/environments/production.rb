24499d8337f66b3d531c12e6f24cadd01488c083
Daniel Sherson <me@danielsherson.com>
2015-02-07 19:54:14 +0000

    please can I have email?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address    => "smtp.sendgrid.net",
     :port       => '587',
-    :authentication  => :plain
+    :authentication  => :plain,
     :user_name  => ENV['SENDGRID_USERNAME'],
     :password   => ENV['SENDGRID_PASSWORD'],
     :domain     => ENV['SENDGRID_DOMAIN'],
